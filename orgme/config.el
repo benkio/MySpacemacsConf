5ffefa50676580ef33f0b8ff6094830520ab8a79
benkio <benkio89@gmail.com>
2016-09-16 14:47:07 +0200

    First commit, add orgme layer.

---
+++ orgme/config.el
@@ -0,0 +1,15 @@
+(add-hook
+ 'org-load-hook
+ (lambda ()
+   (require 'ox-latex)
+   (setq org-latex-default-class "org-book")
+   (add-to-list 'org-latex-classes
+                '("org-book"
+                  "\\documentclass{book}"
+                  ("\\chapter{%s}" . "\\chapter*{%s}")
+                  ("\\section{%s}" . "\\section*{%s}")
+                  ("\\subsection{%s}" . "\\subsection*{%s}")
+                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))))
+
