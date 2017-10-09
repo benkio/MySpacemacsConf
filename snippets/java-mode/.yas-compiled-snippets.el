67484931b8d56314e007dcf4cd936d2cc69bdb77
Enrico <enricobenini@intigris.nl>
2017-10-09 11:03:45 +0200

    Add the pre method comment snippet Update the readme.md

---
+++ snippets/java-mode/.yas-compiled-snippets.el
@@ -0,0 +1,9 @@
+;;; Compiled snippets and support files for `java-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'java-mode
+                     '(("PreMethodComment" "    ///\n    ///\n    /// @param\n    /// @return\n    ///" "PreMethodComment" nil nil nil "/Users/enrico/.emacs.d/private/snippets/java-mode/PreMethodComment" nil nil)
+                       ("CCH" "/**\n * ${1:`(file-name-base buffer-file-name)`}\n *\n * Description       :   \n * Project           :   \n * Version           :   \n * Date              :   `(format-time-string \"%d %B %Y\")`$0\n * Author (original) :   Enrico Benini (enricobenini@)\n **/" "CodeCommentHeader" nil nil nil "/Users/enrico/.emacs.d/private/snippets/java-mode/CodeCommentHeader" nil nil)))
+
+
+;;; Do not edit! File generated at Mon Oct  9 10:45:58 2017
