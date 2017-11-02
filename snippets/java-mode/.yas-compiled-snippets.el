7a4d23bb007ae818630b8ec35f5bba8b90d96cbd
Enrico <enricobenini@intigris.nl>
2017-11-02 13:12:40 +0100

    Try to make the method snippet better, todo: newline and @param

--- snippets/java-mode/.yas-compiled-snippets.el
+++ snippets/java-mode/.yas-compiled-snippets.el
@@ -2,8 +2,8 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'java-mode
-                     '(("PreMethodComment" "    ///\n    ///\n    /// @param\n    /// @return\n    ///" "PreMethodComment" nil nil nil "/Users/enrico/.emacs.d/private/snippets/java-mode/PreMethodComment" nil nil)
-                       ("CCH" "/**\n * ${1:`(file-name-base buffer-file-name)`}\n *\n * Description       :   \n * Project           :   \n * Version           :   \n * Date              :   `(format-time-string \"%d %B %Y\")`$0\n * Author (original) :   Enrico Benini (enricobenini@)\n **/" "CodeCommentHeader" nil nil nil "/Users/enrico/.emacs.d/private/snippets/java-mode/CodeCommentHeader" nil nil)))
+                     '(("mthdoc" "    ///\n    /// $3\n    /// $4\n    /// @return $2 - \n    ///\n${1:private} ${2:void} ${3:name}(${4:args}) {\n    $0\n}" "methodDoc" nil nil nil "/Users/enrico/.emacs.d/private/snippets/java-mode/methodDoc" nil nil)
+                       ("CCH" "/**\n * ${1:`(file-name-base buffer-file-name)`}\n *\n * @description       :   \n * @project           :   \n * @version           :   \n * @date              :   `(format-time-string \"%d %B %Y\")`$0\n * @author (original) :   Enrico Benini (enricobenini@)\n **/" "CodeCommentHeader" nil nil nil "/Users/enrico/.emacs.d/private/snippets/java-mode/CodeCommentHeader" nil nil)))
 
 
-;;; Do not edit! File generated at Mon Oct  9 10:45:58 2017
+;;; Do not edit! File generated at Thu Nov  2 13:12:14 2017
