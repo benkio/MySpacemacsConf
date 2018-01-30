d4c6231adf6a0baaa0b2281502fd93418cb5f0cd
Enrico <enricobenini@intigris.nl>
2018-01-30 12:52:07 +0100

    Add the, 'copy filename to clipboard' function

--- custom/funcs.el
+++ custom/funcs.el
@@ -8,3 +8,13 @@
     (shell-command-on-region (mark) (point) "xmllint --format -" (buffer-name) t)
     )
   )
+
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
