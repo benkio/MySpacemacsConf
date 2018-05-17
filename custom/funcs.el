6f1644b1f7b09c1e4467aaf1c87e9efce45d00fe
Enrico Benini <ebenini@mdsol.com>
2018-05-17 11:39:05 +0100

    Add the delete file & buffer function

--- custom/funcs.el
+++ custom/funcs.el
@@ -60,3 +60,15 @@ https://github.com/jaypei/emacs-neotree/pull/110"
    (reverse
     (split-string
      (delete-and-extract-region beg end) "\\b"))))
+
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
